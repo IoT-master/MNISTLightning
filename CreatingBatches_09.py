b2120de92b675602c6f62916f97f5a6e14a075fb
Senh Mo Chuang <senhmo@gmail.com>
2020-05-29 20:09:13 -0500

    Creating Batches from the Dataset

---
+++ CreatingBatches_09.py
@@ -0,0 +1,33 @@
+from torch.utils.data import DataLoader
+import platform
+import torch
+from CreateDataset_02 import MNISTDataset, ToTensor
+import torchvision
+
+torch.set_printoptions(linewidth=120)
+torch.set_grad_enabled(True)
+
+train_dataset = MNISTDataset(
+    'train.csv', 'Dataset/MNISTDataSet/train', transform=ToTensor())
+
+batch_loader_params = {
+    "batch_size": 50,
+    "shuffle": True,
+    "num_workers": 0 if platform.system() == 'Windows' else 2
+}
+train_batches = DataLoader(train_dataset, **batch_loader_params)
+
+if __name__ == '__main__':
+    batch_samples = iter(train_batches)
+    samples = batch_samples.next()
+    print(samples['image'].shape)
+    datset_batch = torchvision.utils.make_grid(samples['image'])
+    import matplotlib.pyplot as plt
+    import numpy as np
+
+    def imshow(img):
+        npimg = img.numpy()
+        plt.imshow(np.transpose(npimg, (1, 2, 0)))
+        plt.show()
+    print(samples['label'])
+    imshow(torchvision.utils.make_grid(datset_batch))
